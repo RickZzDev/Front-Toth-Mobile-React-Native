63250f10fe810399457f550ecd460f07b61339b6
Carlos <rick_teloon@hotmail.com>
2020-07-20 17:14:37 -0300

    Finalizando menu de turmas

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,15 +7,15 @@ import {
   Animated,
   Easing,
   StatusBar,
+  AsyncStorage,
+  ActivityIndicator,
 } from "react-native";
 import MenuCard from "../../components/HomeComponents/menuCard";
 import BoxRetangular from "../../components/HomeComponents/boxRetangular";
 import { useRoute, useNavigation } from "@react-navigation/native";
+import api from "../../services/api";
+import LottieView from "lottie-react-native";
 
-//Teste2
-//Teste
-//Teste2
-//Teste
 const Home = () => {
   const routes = useRoute();
   const routeParams = routes.params;
@@ -39,9 +39,26 @@ const Home = () => {
   }, []);
 
   const [user, setUser] = useState({});
+  const [turmas, setTurmas] = useState(null);
 
   useEffect(() => {
     setUser(routeParams.data.professor);
+    async function getTurmas() {
+      const token = await AsyncStorage.getItem("jwt_key");
+      const headers = { Authorization: "Bearer " + token };
+      await api
+        .get(`turmas/professores/${routeParams.data.professor.id}`, {
+          headers: headers,
+        })
+        .then((response) => {
+          setTurmas(response.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
+
+    getTurmas();
   }, []);
 
   const transfomrStyleX = {
@@ -53,82 +70,109 @@ const Home = () => {
   };
 
   return (
-    <Animated.View style={{ ...styles.container }}>
-      <StatusBar backgroundColor="white"></StatusBar>
-      <Animated.Text
-        style={{ ...styles.helloText, ...transfomrStyleX, opacity: fadeAnim }}
-      >
-        Olá, {user.nome}
-      </Animated.Text>
-      <Animated.Text
-        style={{ ...styles.welcomText, ...transfomrStyleX, opacity: fadeAnim }}
-      >
-        Seja bem vindo ao Toth
-      </Animated.Text>
-      <Animated.View
-        style={{
-          ...styles.dayliCardContainer,
-          ...transfomrStyleX,
-          opacity: fadeAnim,
-        }}
-      >
-        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-          <View style={styles.dayliCards}>
-            <Text style={styles.dayliCardTitle}>Aulas do dia</Text>
+    <View style={{ flex: 1 }}>
+      {turmas != null ? (
+        <Animated.View style={{ ...styles.container }}>
+          <StatusBar backgroundColor="white"></StatusBar>
+          <Animated.Text
+            style={{
+              ...styles.helloText,
+              ...transfomrStyleX,
+              opacity: fadeAnim,
+            }}
+          >
+            Olá, {user.nome}
+          </Animated.Text>
+          <Animated.Text
+            style={{
+              ...styles.welcomText,
+              ...transfomrStyleX,
+              opacity: fadeAnim,
+            }}
+          >
+            Seja bem vindo ao Toth
+          </Animated.Text>
+          <Animated.View
+            style={{
+              ...styles.dayliCardContainer,
+              ...transfomrStyleX,
+              opacity: fadeAnim,
+            }}
+          >
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              <View style={styles.dayliCards}>
+                <Text style={styles.dayliCardTitle}>Aulas do dia</Text>
 
-            <View style={styles.routineContainer}>
-              <BoxRetangular text="3C 09:30" width={135} />
-              <BoxRetangular text="3C 09:30" width={135} />
-              <BoxRetangular text="3C 09:30" width={135} />
-              <BoxRetangular text="3C 09:30" width={135} />
-              <BoxRetangular text="3C 09:30" width={135} />
-            </View>
+                <View style={styles.routineContainer}>
+                  <BoxRetangular text="3C 09:30" width={135} />
+                  <BoxRetangular text="3C 09:30" width={135} />
+                  <BoxRetangular text="3C 09:30" width={135} />
+                  <BoxRetangular text="3C 09:30" width={135} />
+                  <BoxRetangular text="3C 09:30" width={135} />
+                </View>
+              </View>
+
+              <View style={styles.dayliCards}>
+                <Text style={{ ...styles.dayliCardTitle }}>Comunicados</Text>
+                <BoxRetangular text="Comunicados Gerais" width={185} />
+                <BoxRetangular text="Relatório de dúvidas" width={185} />
+                <BoxRetangular text="Infromações extras" width={185} />
+                <BoxRetangular text="Turmas" width={185} />
+              </View>
+
+              <View style={styles.dayliCards}>
+                <Text style={styles.dayliCardTitle}>Atividades do dia</Text>
+
+                <View style={styles.routineContainer}>
+                  <BoxRetangular text="3A" width={135} />
+                  <BoxRetangular text="3B" width={135} />
+                  <BoxRetangular text="3C" width={135} />
+                  <BoxRetangular text="3D" width={135} />
+                  <BoxRetangular text="3E" width={135} />
+                </View>
+              </View>
+            </ScrollView>
+          </Animated.View>
+          <View style={styles.menuCardsContainer}>
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              <MenuCard text="Atividades" materialIconName="assignment" />
+              <MenuCard
+                dataTurmas={turmas}
+                text="Turmas"
+                materialIconName="school"
+              />
+              <MenuCard
+                text="Comunicados"
+                materialIconName="sms"
+                data={routeParams.data.professor}
+              />
+              <MenuCard
+                text="Chamada"
+                materialIconName="recent-actors"
+                data={user}
+              />
+              <MenuCard
+                text="Notas"
+                materialIconName="graphic-eq"
+                data={user}
+              />
+              <MenuCard text="Provas" materialIconName="event" data={user} />
+            </ScrollView>
           </View>
-
-          <View style={styles.dayliCards}>
-            <Text style={{ ...styles.dayliCardTitle }}>Comunicados</Text>
-            <BoxRetangular text="Comunicados Gerais" width={185} />
-            <BoxRetangular text="Relatório de dúvidas" width={185} />
-            <BoxRetangular text="Infromações extras" width={185} />
-            <BoxRetangular text="Turmas" width={185} />
-          </View>
-
-          <View style={styles.dayliCards}>
-            <Text style={styles.dayliCardTitle}>Atividades do dia</Text>
-
-            <View style={styles.routineContainer}>
-              <BoxRetangular text="3A" width={135} />
-              <BoxRetangular text="3B" width={135} />
-              <BoxRetangular text="3C" width={135} />
-              <BoxRetangular text="3D" width={135} />
-              <BoxRetangular text="3E" width={135} />
-            </View>
-          </View>
-        </ScrollView>
-      </Animated.View>
-      <View style={styles.menuCardsContainer}>
-        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-          <MenuCard text="Atividades" materialIconName="assignment" />
-          <MenuCard
-            dataTurmas={user.turmas}
-            text="Turmas"
-            materialIconName="school"
-          />
-          <MenuCard
-            text="Comunicados"
-            materialIconName="sms"
-            data={routeParams.data.professor}
-          />
-          <MenuCard
-            text="Chamada"
-            materialIconName="recent-actors"
-            data={user}
-          />
-          <MenuCard text="Notas" materialIconName="graphic-eq" data={user} />
-          <MenuCard text="Provas" materialIconName="event" data={user} />
-        </ScrollView>
-      </View>
-    </Animated.View>
+        </Animated.View>
+      ) : (
+        <View
+          style={{
+            backgroundColor: "white",
+            alignItems: "center",
+            justifyContent: "center",
+            flex: 1,
+          }}
+        >
+          <LottieView autoPlay loop source={require("./urson2.json")} />
+        </View>
+      )}
+    </View>
   );
 };
 
